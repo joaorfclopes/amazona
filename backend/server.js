8427862ca4673c4becb2b1ced6e02227c7c79fa3
João Lopes <joaorfclopes@gmail.com>
2020-11-01 17:05:04 +0000

    Publish to Heroku

--- backend/server.js
+++ backend/server.js
@@ -28,13 +28,18 @@ app.use("/api/uploads", uploadRouter);
 const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
+app.use(express.static(path.join(__dirname, "/frontend/build")));
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "/frontend/build/index.html"))
+);
+
 app.get("/api/config/paypal", (req, res) => {
   res.send(process.env.PAYPAL_CLIENT_ID || "sb");
 });
 
-app.get("/", (req, res) => {
-  res.send("Server is ready");
-});
+//app.get("/", (req, res) => {
+//  res.send("Server is ready");
+//});
 
 app.use((err, req, res) => {
   res.status(500).send({ message: err.message });
