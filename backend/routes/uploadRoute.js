77263aedb599efe69761f673cb36b65a7941a80c
Joao Lopes <joaorfclopes@gmail.com>
2020-11-09 20:05:28 +0000

    Upload Product Images On AWS S3

--- backend/routes/uploadRoute.js
+++ backend/routes/uploadRoute.js
@@ -1,5 +1,7 @@
 import express from "express";
 import multer from "multer";
+import multerS3 from "multer-s3";
+import aws from "aws-sdk";
 
 const storage = multer.diskStorage({
   destination(req, file, cb) {
@@ -16,6 +18,27 @@ const uploadRouter = express.Router();
 
 uploadRouter.post("/", upload.single("image"), (req, res) => {
   res.send(`/${req.file.path}`);
+});
+
+aws.config.update({
+  accessKeyId: process.env.accessKeyId,
+  secretAccessKey: process.env.secretAccessKey,
+});
+
+const s3 = new aws.S3();
+const storageS3 = multerS3({
+  s3,
+  bucket: "new-amazona-bucket",
+  acl: "public-read",
+  contentType: multerS3.AUTO_CONTENT_TYPE,
+  key(req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+const uploadS3 = multer({ storage: storageS3 });
+
+uploadRouter.post("/s3", uploadS3.single("image"), (req, res) => {
+  res.send(req.file.location);
 });
 
 export default uploadRouter;
